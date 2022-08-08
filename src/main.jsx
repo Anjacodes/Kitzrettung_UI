18bdc4ad54e65a4ff55a0f68cee11d87fde8e37e
Anjacodes <anja_schmidt7@posteo.de>
2022-08-08 14:54:57 +0200

    Add Scroll To Top on Page Change

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Footer from './components/sharedComponents/Footer'
+import ScrollToTop from './components/sharedComponents/scrollToTop';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
       <Footer />
     </BrowserRouter>
