77975884cc23724b343e43d7f8c35ad2c1d0ff19
Anjacodes <anja_schmidt7@posteo.de>
2022-08-10 17:21:10 +0200

    Fix Linters

--- src/components/sharedComponents/GeneralCover.jsx
+++ src/components/sharedComponents/GeneralCover.jsx
@@ -5,8 +5,6 @@ function GeneralCover() {
   const pathName = useLocation();
   const current = pathName.pathname.slice(1).trim();
 
-  console.log(current);
-
   return (
     <div className={`h-[50vh] bg-[url('../../src/assets/${current}.jpg')] bg-cover bg-neutral-700 bg-blend-overlay flex flex-col justify-center items-center bg-center`}>
     </div>
