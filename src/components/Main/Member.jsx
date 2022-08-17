1557c5c0e18e62b8f97a81966ad68712008a02e6
Anjacodes <anja_schmidt7@posteo.de>
2022-08-17 12:41:13 +0200

    Implement Captcha, Conditional Form Styling

--- src/components/Main/Member.jsx
+++ src/components/Main/Member.jsx
@@ -4,9 +4,9 @@ import Form from '../sharedComponents/Form'
 function Member() {
   return (
     <section className="w-screen -ml-20 lg:-ml-52 mt-48 h-[30rem] relative bg-[url('https://images.unsplash.com/photo-1473603477862-9d352d4615e1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80')] bg-cover bg-neutral-700 bg-blend-overlay">
-        <div className="absolute top-[5%] lg:top-[30%] flex flex-col items-center w-full">
+        <div className="absolute top-[10%] lg:top-[30%] flex flex-col items-center w-full">
           <h3 className="text-white text-4xl tracking-wide">WERDE MITGLIED</h3>
-          <p className="w-2/3 lg:w-1/2 text-white text-xl text-center mt-6 mb-6 lg:mb-12">Trage deinen Namen und Email Adresse ein und erhalte Informationen zur Mitgliedschaft und deinen Antrag per Email.</p>
+          <p className="w-1/2 text-white text-xl text-center mt-6 mb-12">Fordere Informationen zur Mitgliedschaft und deinen Antrag per Email an.</p>
           <Form />
         </div>
     </section>
--- src/components/sharedComponents/Footer.jsx
+++ src/components/sharedComponents/Footer.jsx
@@ -4,7 +4,6 @@ import logo from '../../assets/Rehkitz_negativ.png'
 import Form from './Form';
 
 function Footer() {
-
   self.displayName = "Footer";
 
   return (
@@ -12,7 +11,7 @@ function Footer() {
       <div className="mx-[5vw] pt-6 flex flex-col h-full justify-between lg:grid lg:grid-cols-2 lg:gap-20 relative">
         <div className="flex">
           <div className="text-sm mr-6 lg:mr-12 flex flex-col group">
-            <Link to="/about" className="mb-4 text-base font-avenirHeavy hover:text-first hover:cursor-pointer">ÜBER UNS</Link>
+            <a className="mb-4 text-base font-avenirHeavy hover:text-first hover:cursor-pointer">ÜBER UNS</a>
             <ul className='hidden group-hover:block lg:block'>
               <li><NavLink to="/motivation" className="hover:text-first hover:cursor-pointer">Motivation</NavLink></li>
               <li><NavLink to="/verein" className="hover:text-first hover:cursor-pointer">Verein</NavLink></li>
@@ -29,12 +28,12 @@ function Footer() {
           </div>
           <div className="text-sm mr-6 lg:mr-12 flex flex-col group">
             <a className="mb-4 text-base font-avenirHeavy hover:text-first hover:cursor-pointer">WISSENSWERTES</a>
-            <ul className='hidden group-hover:block lg:block'>
+           <ul className='hidden group-hover:block lg:block'>
               <li><NavLink to="/biologie" className="hover:text-first hover:cursor-pointer">Biologie</NavLink></li>
               <li><NavLink to="/technik" className="hover:text-first hover:cursor-pointer">Technik</NavLink></li>
               <li><NavLink to="/galerie" className="hover:text-first hover:cursor-pointer">Galerie</NavLink></li>
-              <li><NavLink to="/" className="hover:text-first hover:cursor-pointer">Spenden</NavLink></li>
-            </ul>
+              <li><NavLink to="/" className=" hover:text-first hover:cursor-pointer">Spenden</NavLink></li>
+           </ul>
           </div>
         </div>
         <div className='flex justify-between mt-auto lg:mt-0'>
@@ -47,15 +46,16 @@ function Footer() {
           </div>
           <div className='w-1/2'>
             <h3 className='mb-1 text-white'>WERDE MITGLIED</h3>
-            <p className='text-sm mb-1'>Fordere Informationen zur Mitgliedschaft und deinen Antrag per Email an</p>
+            <p className='text-sm mb-1'>Fordere Informationen zur Mitgliedschaft und deinen Antrag per Email an.</p>
             <Form displayName={displayName}/>
+           
           </div>
         </div>
         <div className="flex items-center absolute top-80 lg:top-48">
-          <Link to="/" className="hover:cursor-pointer mr-4"><img src={logo} className="w-20 lg:w-32"/></Link>
+          <Link to="/" className="hover:cursor-pointer mr-4"><img src={logo} className="w-20 lg:w-36"/></Link>
           <div>
-            <p className="text-sm hidden lg:block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p className="mt-4 lg:mt-0 text-sm hidden lg:block">2022 Kreis Euskirchen Rehkitzrettung eV</p>
+            <p className="text-sm hidden lg:block">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p className="hidden lg:block mt-4 lg:mt-0 text-sm">2022 Kreis Euskirchen Rehkitzrettung eV</p>
           </div>
         </div>
       </div>
