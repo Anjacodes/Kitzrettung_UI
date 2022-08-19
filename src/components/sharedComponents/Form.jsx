af45905d66a06274dcf400ff50e97fde1e132f05
Anjacodes <anja_schmidt7@posteo.de>
2022-08-19 09:34:01 +0200

    Remove Commented Text and Console.Logs

--- src/components/sharedComponents/Form.jsx
+++ src/components/sharedComponents/Form.jsx
@@ -1,16 +1,10 @@
 import React, { useState } from 'react'
-// import ReCAPTCHA from "react-google-recaptcha"
-// import { useForm } from '@formspree/react';
 
 function Form({displayName}) {
 
   const [isVerified, setIsVerified] = useState(false);
   const [hasInfo, setHasInfo] = useState(false);
   const [isDesktop] = useState(window.innerWidth > 650);
-  // const [state] = useForm("xknenklz");
-  // if (state.succeeded) {
-  //     return <p>Thanks for joining!</p>;
-  // }
 
   const handleOnChange = () => {
     setIsVerified(true);
@@ -19,8 +13,6 @@ function Form({displayName}) {
   const handleOnChangeForm = () => {
     setHasInfo(true);
   }
-
-  console.log(displayName)
 
   return (
     <form className="text-center flex flex-col text-base lg:mb-4" action="https://formspree.io/f/xknenklz" method="POST">
