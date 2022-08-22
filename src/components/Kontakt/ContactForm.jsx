b2515a5328402d6f9e8ab6a785580275f78e810c
Anjacodes <anja_schmidt7@posteo.de>
2022-08-22 12:56:16 +0200

    Change Button Hover Color, Fix Linters

--- src/components/Kontakt/ContactForm.jsx
+++ src/components/Kontakt/ContactForm.jsx
@@ -21,7 +21,7 @@ function ContactForm() {
       <input className="rounded-lg py-2 px-5 mb-4 w-1/2 bg-slate-100" type="email" name="email" placeholder='Your email...' value={email} onChange={event => {setEmail(event.target.value)}} />
       <textarea className="rounded-lg py-2 px-5 mb-4 w-1/2 bg-slate-100 h-40" name="message" type="textarea" placeholder='Your message...' value={message} onChange={event => {setMessage(event.target.value)}} />
       <div className="g-recaptcha" data-sitekey="6LfZmX0hAAAAAPRqwlBGUIlqKnDJ9KSL-JS5xxa5"></div>
-      <button type="submit" className="bg-first mt-2 rounded-lg py-2 px-4 text-white text-base hover:bg-fourth w-1/2">Absenden</button>
+      <button type="submit" className="bg-first mt-2 rounded-lg py-2 px-4 text-white text-base hover:bg-third w-1/2">Absenden</button>
 </form>
   )
 }
