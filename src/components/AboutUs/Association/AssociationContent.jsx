a591879ed6c7dfb6b821105217bca97a51d8b563
Anjacodes <anja_schmidt7@posteo.de>
2022-08-24 07:38:46 +0200

    Remove Vereinssatzung, Remove Double Paragraph

--- src/components/AboutUs/Association/AssociationContent.jsx
+++ src/components/AboutUs/Association/AssociationContent.jsx
@@ -6,22 +6,18 @@ import satzung from '../../../assets/Vereinssatzung-Rehkitz.pdf'
 function AssociationContent() {
 
   return (
-    <div className="mx-20 my-48 lg:mx-52">
+    <div className="mx-20 my-48 lg:mx-52 text-base text-justify text-slate-600">
       <h2 className="mb-6 text-2xl tracking-wide">UNSER VEREIN</h2>
-      <p className="text-base text-justify text-slate-600 my-4">In den letzten zwei Jahren wurde die Rehkitzrettung durch privates Engagement verschiedener Mitglieder des jetzigen Vereins im Raum Bad Münstereifel und Euskirchen durchgeführt. Dabei wurden für die Beschaffung und den Betrieb einer Wärmebilddrohne nur private finanzielle Mittel genutzt und in 15 Missionen über 25 Rehkitze gerettet.</p>
-      <div className='mt-12 lg:flex'>
-        <img src={Arbeit} className="w-1/3 hidden lg:block"/>
-        <div className='lg:ml-12 text-base text-justify text-slate-600 my-4 w-full lg:w-2/3'>
-          <p className="mb-6">Leider mussten auch viele Anfragen aufgrund der limitiert verfügbaren Technik (es stand nur eine Drohne zur Verfügung) zurückgewiesen werden. Wir gehen daher davon aus, dass weitere Kitze hätten gerettet werden können.</p>
+      <p className="my-4">In den letzten zwei Jahren wurde die Rehkitzrettung durch privates Engagement verschiedener Mitglieder des jetzigen Vereins im Raum Bad Münstereifel und Euskirchen durchgeführt. Dabei wurden für die Beschaffung und den Betrieb einer Wärmebilddrohne nur private finanzielle Mittel genutzt und in 15 Missionen über 25 Rehkitze gerettet.</p>
 
-          <p className="mb-6">Um die Idee der Rehkitzrettung mit Wärmebilddrohnen weiter zu verbreiten und um insbesondere die Anschaffung weiterer Drohnen finanzieren zu können (eine gut geeignete Drohne kostet derzeit rund 6.500 Euro), haben wir diesen Verein gegründet. Wir hoffen darauf, so eine Vielzahl weiterer Tiere vor dem Tod durch landwirtschaftliche Maschinen retten zu können.</p>
+      <p className="mb-6">Leider mussten auch viele Anfragen aufgrund der limitiert verfügbaren Technik (es stand nur eine Drohne zur Verfügung) zurückgewiesen werden. Wir gehen daher davon aus, dass weitere Kitze hätten gerettet werden können.</p>
 
-          <p className="mb-10 lg:mb-20">Um die Idee der Rehkitzrettung mit Wärmebilddrohnen weiter zu verbreiten und um insbesondere die Anschaffung weiterer Drohnen finanzieren zu können (eine gut geeignete Drohne kostet derzeit rund 6.500 Euro), haben wir diesen Verein gegründet. Wir hoffen darauf, so eine Vielzahl weiterer Tiere vor dem Tod durch landwirtschaftliche Maschinen retten zu können.</p>
+      <p className="mb-10">Um die Idee der Rehkitzrettung mit Wärmebilddrohnen weiter zu verbreiten und um insbesondere die Anschaffung weiterer Drohnen finanzieren zu können (eine gut geeignete Drohne kostet derzeit rund 6.500 Euro), haben wir diesen Verein gegründet. Wir hoffen darauf, so eine Vielzahl weiterer Tiere vor dem Tod durch landwirtschaftliche Maschinen retten zu können.</p>
 
-          <a href={satzung} target="_blank" rel="noreferrer" className='bg-first py-2 px-3 lg:px-6 rounded-xl text-base text-white hover:bg-third '>Vereinssatzung ansehen</a>
-        </div>
-      </div>
-      <div className='mt-20'>
+      <a href={satzung} target="_blank" rel="noreferrer" className='bg-first hidden py-2 px-3 lg:px-6 rounded-xl text-base text-white hover:bg-third '>Vereinssatzung ansehen</a>
+
+
+      <div className='mt-40'>
 
         <h3 className='mb-12 text-2xl tracking-wide'>VORSTAND</h3>
 
