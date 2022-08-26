9e548fb81ba5199e7f1723d0f46e8f86ef8fc7b0
Anjacodes <anja_schmidt7@posteo.de>
2022-08-26 19:15:49 +0200

    Add Impressum and Datenschutz Raw Text

---
+++ src/components/Impressum_Datenschutz/Impressum.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import GeneralCover from '../sharedComponents/GeneralCover';
+
+function Impressum() {
+  return (
+    <div>
+      <GeneralCover />
+      <p>
+      IMPRESSUM 
+
+Verantwortlich für den Inhalt: 
+
+ 
+
+Rehkitzrettung Kreis Euskirchen e.V. 
+
+Hanna-Nagel-Str. 12 
+
+53879 Euskirchen 
+
+ 
+
+Telefon: 0151 74254953 
+
+ 
+
+eMail: vorstand@rehkitzrettung-euskirchen.de 
+
+Internet: www.rehkitzrettung-euskirchen.de 
+
+ 
+
+Vertretungsberechtig sind zwei Mitglieder des Vorstands 
+
+ 
+
+Registergericht: Amtsgericht Bonn 
+
+Vereinsnummer: VR 11878 
+
+Steuer-ID-Nr.: 209/5728/0782 
+
+Die Gemeinnützigkeit des Vereins wurde vom Finanzamt Euskirchen anerkannt und bestätigt. 
+
+ 
+
+Für die Inhalte auf dieser Webseite ist der Vorstand verantwortlich. 
+
+ 
+
+Fotos: Urhebungsrechte und Copyright der veröffentlichten Bilder und Videos liegen beim Verein Rehkitzrettung Kreis Euskirchen e.V.
+
+Unsere Satzung finden Sie hier (Link)
+
+Rechtlicher Hinweis: Mit Urteil vom 12. Mai 1998 hat das Landgericht Hamburg entschieden, dass man durch die Ausbringung eines Links die Inhalte der gelinkten Seite unter Umständen mit zu verantworten hat. Dies kann, so das LG, nur dadurch verhindert werden, dass man sich ausdrücklich von diesen Inhalten distanziert. Wir erklären hiermit ausdrücklich, dass wir keinen Einfluss auf die Gestaltung und den Inhalt der gelinkten Seiten haben. Deshalb distanzieren wir uns hiermit ausdrücklich von allen Inhalten aller gelinkten Seiten dieser Homepage und macht sich deren Inhalt nicht zu Eigen. Diese Erklärung gilt für alle auf dieser Page angezeigten Links und für alle Inhalte der Seiten, zu denen die hier aufgeführten Links führen. 
+      </p>
+    </div>
+  )
+}
+
+export default Impressum;
