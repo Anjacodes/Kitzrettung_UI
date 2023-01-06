63b9eafc642fb09a612bdafcac53fea5040cb583
Anjacodes <anja_schmidt7@posteo.de>
2023-01-06 16:55:35 +0100

    Add Biologie

---
+++ src/components/Info/Biologie/BiologieContent.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+function BiologieContent() {
+    return (
+      <div className="mx-20 my-48 lg:mx-52">
+      <h2 className="mb-6 text-2xl tracking-wide">Biologie</h2>
+      <div className='mt-12'>
+        <div className='text-base text-justify text-slate-600 my-4'>
+          <p className="mb-6">Das Rehwild ist die in Mitteleuropa am häufigsten vorkommende Hirschart.</p>
+          <p className="mb-6">Rehwild besiedelte ursprünglich Waldrandzonen und -lichtungen, es hat sich mittlerweile aber eine Reihe sehr unterschiedlicher Habitate erschlossen und kommt mittlerweile auch in offenen, fast deckungslosen landwirtschaftlichen Flächen vor. Aufgeschreckte Rehe suchen normalerweise mit wenigen, schnellen Sprüngen Schutz in Dickungen, Hecken und Wäldern. Das Reh ist eine sehr anpassungsfähige Art und hat sich fast alle natürlichen Lebensräume erschlossen, die in Europa zu finden sind. Die Höhenverbreitung reicht von der Tiefebene bis in alpine Höhenlagen von 3.000 Metern.</p>
+
+          <p className="mb-6">Das erwachsene männliche Reh wird als Rehbock (auch Bock) bezeichnet, das erwachsene weibliche Stück als Ricke. Die nicht-einjährigen Jungtiere werden als Rehkitze bezeichnet. Einjährige weibliche Tiere werden Schmalrehe genannt.</p>
+  
+          <p className="mb-6">Rehe sind Wiederkäuer und werden als Konzentratselektierer bezeichnet, da sie bevorzugt eiweißreiches Futter äsen. Während des Sommerhalbjahrs leben Rehe überwiegend einzeln oder in Familientrupps, die aus einer Ricke und ihren Kitzen bestehen. Im Winter bilden sich sogenannte Sprünge, die meist nicht mehr als drei oder vier Tiere verschiedenen Geschlechts umfassen.</p>
+  
+          <p className="mb-6">Das Rehwild unterliegt dem Jagdrecht und wird dem <a href={"https://wildeswissen.de/2015/09/07/schalenwild/"} target="_blank" rel="noopener" className="text-blue-600 hover:underline">Schalenwild</a> zugeordnet. Die Jagdstrecke beträgt allein auf dem Gebiet Deutschlands jährlich mehr als eine Million Stück.</p>
+       </div>
+      </div>
+
+      <div className="text-base text-justify align-middle text-slate-600 mt-5">
+        <p className="mb-6">Das Reh ist ein ausgeprägt geruchlich orientiertes Tier. Rehe sind in der Lage, bereits geringe Duftreize wahrzunehmen und riechen einen Menschen aus einer Entfernung von 300 bis 400 Metern. Die seitlich stehenden Augen erlauben dem Reh ohne Kopfdrehung einen weiten Umkreis zu überblicken. Rehe reagieren besonders auf Bewegungen, das Erkennungsvermögen für unbewegte Gegenstände ist nicht sehr hoch entwickelt.</p>
+
+        <h2 className="mt-10 mb-6 text-2xl tracking-wide">Fortpflanzung</h2>
+        
+        <p className="mb-6">Bei Rehen kommt es zur sogenannten Keimruhe. Das befruchtete Ei entwickelt sich (nach der Brunft im Juli/August) erst ab Dezember und führt in Deutschland zur Geburt der Kitze vorwiegend im Mai und Juni des folgenden Jahres, die Gesamttragezeit beträgt durchschnittlich neuneinhalb Monate.
+        </p>
+        <p className="mb-6">Kurz vor der Geburt (dem Setzen) sucht die Ricke einen Setzplatz aus, meist eine wenig bewachsene Stelle in der Nähe von Waldrändern und Hecken. In Heugraswiesen sind die Setzplätze etwa zwei Quadratmeter große Flächen, in denen die Ricken das Gras niedergewälzt haben. Ricken können zwischen einem und vier Jungen zur Welt bringen. In der Regel sind es bis drei zu Kitzen. Die Kitze einer Ricke suchen keine gemeinsamen Liegeplätze auf, sondern befinden nach der Geburt meist zwanzig bis achtzig Meter voneinander entfernt.</p>
+        <p className="mb-6">
+        Bei Störungen wie rasche Bewegungen in ihrer Nähe, Lärm oder fremden Geruch verharren Kitze starr an den Boden gepresst. Ein Fluchtverhalten setzt erst im Alter von drei bis vier Wochen ein.
+        Dieses, als Duckreflex bezeichnete, Verhalten in Verbindung mit der Tatsache, dass sie nahezu keinen Geruch abgeben sowie ihrer Tarnung sichert den Kitzen gegenüber ihren natürlichen Feinden das Überleben.
+        </p>
+        <p className="mb-6">Das gleiche Verhalten bedeutet aber für ca. 100.000 Kitze in Deutschland jährlich den Tod durch Mähmaschinen während der Heuernte.</p>
+      </div>
+    </div>
+    )
+  }
+
+  export default BiologieContent
