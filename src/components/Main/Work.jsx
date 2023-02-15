57276a155433acc859cb796cf1d1e64638a46358
Anjacodes <anja_schmidt7@posteo.de>
2023-02-15 20:58:37 +0100

    Removed green blocks on Main

--- src/components/Main/Work.jsx
+++ src/components/Main/Work.jsx
@@ -14,7 +14,6 @@ function Work() {
             <p className="text-base text-justify text-slate-600 my-4">Unser eingetragener Verein hat es sich gemäß seiner Satzung zur Aufgabe gemacht das Tierleid, das durch den Einsatz landwirtschaftlicher Maschinen entsteht, möglichst zu verhindern (siehe <a href={Satzung} target="_blank" rel="noreferrer" className='text-blue-600 hover:underline'>Satzung</a>). Wir setzen dabei auf den Einsatz von Drohnen mit hochauflösender Wärmebildtechnik (siehe <NavLink to="/technik" className='text-blue-600 hover:underline'>Technik</NavLink>)</p>
             <NavLink to="/arbeit" className="bg-first py-2 px-6 rounded-xl text-base text-white hover:bg-third">Mehr erfahren</NavLink>
         </div>
-        <div className="bg-first opacity-25 absolute w-[25%] h-[100%] left-[35%] hidden lg:block"></div>
         <img src={Credo} className="hidden lg:block lg:w-[50%]"/>
       </div>
 
@@ -25,7 +24,6 @@ function Work() {
             <p className="text-base text-justify text-slate-600 my-4">Unser Verein nutzt Drohnen der Firmen DJI und Yuneec. Diese Drohnen verfügen über eine Echtbildkamera mit großem Zoom in Verbindung mit einer Wärmebildkamera. Die Wärmebildkameras besitzen Auflösungen von 320x160 Pixeln oder 640x320 Pixeln. </p>
             <NavLink to="/technik" className="bg-first py-2 px-6 rounded-xl text-base text-white hover:bg-third">Mehr erfahren</NavLink>
         </div>
-        <div className="bg-first opacity-25 absolute hidden h-[100%] right-[35%] w-[25%] lg:block"></div>
       </div>
 
       <NavLink to="/arbeit"><p className="text-center mt-20 lg:mt-24 text-sm text-[#45BF41] hover:cursor-pointer hover:text-third">SIEH DIR MEHR ÜBER UNSERE ARBEIT AN  <i className="fa-solid fa-arrow-right-long"></i></p></NavLink>
