a5113ec546afb7e81ca5e6b969501beb55238c07
Anjacodes <anja_schmidt7@posteo.de>
2023-05-26 13:40:29 +0200

    Change Ticker Font

--- src/components/sharedComponents/Ticker.jsx
+++ src/components/sharedComponents/Ticker.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ticker = () => {
     return (
         <div className="bg-third py-4">
-            <h2 className="text-white animate-ticker">Diese Saison: 10 gerettete Rehkitze, 2 Hasen, 1 Fasanengelege </h2>
+            <h2 className="text-white animate-ticker uppercase text-2xl">Diese Saison bereits gerettet: 18 gerettete Rehkitze, 1 Rotwildkalb, 2 Hasen, 1 Fasanengelege </h2>
         </div>
     )
 }
