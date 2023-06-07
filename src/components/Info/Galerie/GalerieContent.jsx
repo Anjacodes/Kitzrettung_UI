a382934f5da7943aef84e8a00d2581dd1d9a4a7d
Anjacodes <anja_schmidt7@posteo.de>
2023-06-07 12:56:47 +0200

    Rename Images

--- src/components/Info/Galerie/GalerieContent.jsx
+++ src/components/Info/Galerie/GalerieContent.jsx
@@ -2,12 +2,12 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Drohne from "/src/assets/Drohne_Flug.jpg";
+import Drohne from '/src/assets/Drohne_Flug.jpg';
 // import Sonnenaufgang from "/src/assets/Sonnenaufgang.jpg";
-import Drohne_fliegt from "/src/assets/Drohne_fliegt.jpg";
-import Rehkitz_aufheben from "/src/assets/Rehkitz_aufheben.jpg";
-import Rehkitz_tragen from "/src/assets/Rehkitz_tragen.jpg";
-import RehkitzII from "/src/assets/Rehkitz_II.jpg";
+import Drohne_fliegt from '/src/assets/Drohne_fliegt.jpg';
+import Rehkitz_aufheben from '/src/assets/Rehkitz_aufheben.jpg';
+import Rehkitz_tragen from '/src/assets/Rehkitz_tragen.jpg';
+import RehkitzII from '/src/assets/Rehkitz_II.jpg';
 
 
 const GalerieContent = () => {
