b6849576a08d27459944ac73b04664229ed7c02a
Anjacodes <anja_schmidt7@posteo.de>
2023-06-16 16:10:14 +0200

    Add Second Blog Post

--- src/components/Main/News.jsx
+++ src/components/Main/News.jsx
@@ -8,24 +8,26 @@ function News() {
   const pathName = useLocation();
   const current = pathName.pathname.slice(1);
 
+  const renderPosts = (blogPosts) => {
+    return blogPosts.map(post => 
+      <article className="mb-20 lg:mb-0" key={post.id}>
+      <Link to={`/blog/${post.id}`}><img src={post.imageURL} className="w-full h-80"/></Link>
+      <h3 className="text-lg mt-4">{post.title}</h3>
+      <div className="flex items-center text-sm text-slate-500 my-3">
+        <p className="mr-6">{post.info.dateCreated}</p>
+      </div>
+      <p className="text-base">{post.shortText}</p>
+      <Link to={`/blog/${post.id}`} ><button className="bg-first py-2 px-10 rounded-xl text-base text-white mt-6 hover:bg-third">Read More</button></Link>
+      </article>
+    )
+  }
+
   return (
     <section className="mt-28">
       <h2 className="text-center text-3xl tracking-wide text-slate-600 mb-16">NEUIGKEITEN</h2>
 
       <div className="lg:grid lg:grid-cols-3 lg:gap-20 w-full md:grid md:grid-cols-2 md:gap-20">
-        {posts.map(post => {
-          return (
-            <article className="mb-20 lg:mb-0" key={post.id}>
-              <Link to={`/blog/${post.id}`}><img src={post.imageURL} className="w-full h-80"/></Link>
-              <h3 className="text-lg mt-4">{post.title}</h3>
-              <div className="flex items-center text-sm text-slate-500 my-3">
-                <p className="mr-6">{post.info.dateCreated}</p>
-              </div>
-              <p className="text-base text-justify">{post.shortText}</p>
-              <Link to={`/blog/${post.id}`} ><button className="bg-first py-2 px-10 rounded-xl text-base text-white mt-6 hover:bg-third">Read More</button></Link>
-            </article>
-          )
-        })}
+        {renderPosts(posts)}
       </div>
         <NavLink to="/blog" className={`${current == "blog" ? "hidden" : ""}`}><p className="text-center mt-20 lg:mt-32 text-sm text-[#45BF41] hover:cursor-pointer hover:text-third">SIEH DIR ALLE NEUIGKEITEN AN  <i className="fa-solid fa-arrow-right-long"></i></p></NavLink>
     </section>
--- src/components/Main/blogPosts.jsx
+++ src/components/Main/blogPosts.jsx
@@ -1,14 +1,25 @@
 import artikel from '/src/assets/Wochenspiegel.png';
+import artikel2 from '/src/assets/Rehkitzrettung_artikel2.jpg';
 
 const posts = [
   {
     id: 1,
-    title: 'BEITRAG IM KÖLNER STADTANZEIGER',
+    title: 'BEITRAG IM KÖLNER WOCHENSPIEGEL',
     imageURL: `${artikel}`,
     info: {
       dateCreated: '19. April 2023',
     },
     shortText: 'Rehkitzrettung: Eine Herausforderung für Landwirte und Jäger gleichermaßen. Ein neuer Verein unterstützt ehrenamtlich.',
+    fullText: '',
+  },
+  {
+    id: 2,
+    title: 'BEITRAG IM EUSKIRCHENER WOCHENBLATT',
+    imageURL: `${artikel2}`,
+    info: {
+      dateCreated: '7. Juni 2023',
+    },
+    shortText: 'Gemeinsam zum Ziel. Landwirt, Jagdausübungsberechtigter und Rehkitzrettungsverein arbeiten effektiv zusammen.',
     fullText: '',
   }
 ];
